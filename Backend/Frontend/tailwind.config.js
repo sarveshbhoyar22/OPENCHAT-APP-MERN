75e6e7062cadabb792331e312b787d8a943ea6ba
SarveshBhoyar <sarveshkbhoyar2022@gmail.com>
2024-12-20 15:58:28 +0530

    daisyui

--- Backend/Frontend/tailwind.config.js
+++ Backend/Frontend/tailwind.config.js
@@ -1,13 +1,22 @@
+// /** @type {import('tailwindcss').Config} */
+// // import daisyui from "daisyui";
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require("daisyui")],
+// };
+// module.exports = {
+//   //...
+//   plugins: [daisyui],
+// };
+
 /** @type {import('tailwindcss').Config} */
-// import daisyui from "daisyui";
-export default {
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
 };
-// module.exports = {
-//   //...
-//   plugins: [daisyui],
-// };

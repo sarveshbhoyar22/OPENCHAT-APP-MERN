d05058bd08c66a4b831eecf9131047eb01b32672
Sarvesh Bhoyar <134962436+sarveshbhoyar22@users.noreply.github.com>
2024-12-20 16:10:25 +0530

    Update tailwind.config.js

--- Backend/Frontend/tailwind.config.js
+++ Backend/Frontend/tailwind.config.js
@@ -14,11 +14,21 @@
 
 /** @type {import('tailwindcss').Config} */
 /** @type {import('tailwindcss').Config} */
+// import daisyui from "daisyui";
+// module.exports = {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require("daisyui")],
+// }
+
 import daisyui from "daisyui";
-module.exports = {
+
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
-}
+  plugins: [daisyui],
+};

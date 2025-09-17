9161827bc9ae0ef4f642320979a14137f89edc45
Sarvesh Bhoyar <134962436+sarveshbhoyar22@users.noreply.github.com>
2025-09-17 13:03:47 +0530

    Update extraxtTime.js

--- Backend/utils/extraxtTime.js
+++ Backend/utils/extraxtTime.js
@@ -1,5 +1,8 @@
 export function extractTime(dateString) {
+  if(!dateString) return "";
   const date = new Date(dateString);
+  if(isNaN(date.getTime())) return "";
+  
   const hours = padZero(date.getHours());
   const minutes = padZero(date.getMinutes());
   return `${hours}:${minutes}`;

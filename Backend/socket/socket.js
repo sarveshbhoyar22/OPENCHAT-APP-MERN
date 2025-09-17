a7fecc27b3ebb7cda41a1301aa5290d3e780286a
Sarveshbhoyar22 <sarveshkbhoyar2022@gmail.com>
2025-09-17 17:02:07 +0530

    updated socket

--- Backend/socket/socket.js
+++ Backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: [import.meta.env.VITE_FRONTEND_URL],
+    origin:process.env.VITE_FRONTEND_URL,
     methods: ["GET", "POST"],
   },
 });
